80d5b7f96fa12c1b538ff85b4a450758af528aa6
reload666 <piloto_66@hotmail.com>
2018-07-20 17:13:58 -0300

    Adicionado suporte Security, script sql

--- src/main/resources/db/migration/mysql/V1__init.sql
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -1,83 +1,49 @@
-CREATE TABLE 'empresa' (
-'id' bigint (20) NOT NULL,
-'cnpj' varchar (255) NOT NULL,
-'data_atualizacao' datetime NOT NULL,
-'data_criacao' datetime NOT NULL,
-'razao_social' varchar (255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+DROP TABLE IF EXISTS `empresa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `empresa` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cnpj` varchar(255) NOT NULL,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `razao_social` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE 'funcionario' (
-'id' bigint (20) NOT NULL,
-'cpf' varchar (255) NOT NULL,
-'data_atualizacao' datetime NOT NULL,
-'data_criacao' datetime NOT NULL,
-'email' varchar (255) NOT NULL,
-'nome' varchar (255) NOT NULL,
-'perfil' varchar (255) NOT NULL,
-'qtd_horas_almoco' float DEFAULT NULL,
-'qtd_horas_trabalho_dia' float DEFAULT NULL,
-'senha' varchar (255) NOT NULL,
-'valor_hora' decimal(19,2) DEFAULT NULL,
-'empresa_id' bigint(20) DEFAULT NULL
-) ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+DROP TABLE IF EXISTS `funcionario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `funcionario` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `cpf` varchar(255) NOT NULL,
+  `data_atualizacao` date NOT NULL,
+  `data_criacao` date NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `perfil` varchar(255) NOT NULL,
+  `qtd_horas_almoco` float DEFAULT NULL,
+  `qtd_horas_trabalho_dia` float DEFAULT NULL,
+  `senha` varchar(255) NOT NULL,
+  `valor_hora` decimal(19,2) DEFAULT NULL,
+  `empresa_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`,`empresa_id`),
+  KEY `fk_funcionario_empresa_idx` (`empresa_id`),
+  CONSTRAINT `fk_funcionario_empresa` FOREIGN KEY (`empresa_id`) REFERENCES `empresa` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE 'lancamento' (
-'id' bigint (20) NOT NULL,
-'data' datetime NOT NULL,
-'data_atualizacao' datetime NOT NULL,
-'data_criacao' datetime NOT NULL,
-'descricao' varchar(255) DEFAULT NULL,
-'localizacao' varchar (255) NOT NULL,
-'tipo' varchar (255) NOT NULL,
-'funcionario_id' bigint(20) DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
-
---
--- Indexes for table 'empresa'
---
-ALTER TABLE 'empresa'
-ADD PRIMARY KEY ('id');
-
---
--- Indexes for table 'funcionario'
---
-ALTER TABLE 'funcionario'
-ADD PRIMARY KEY ('id');
-
-
-
-
-
---
--- AUTO_INCREMENT for table 'empresa'
---
-ALTER TABLE 'empresa'
-MODIFY 'id' bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table 'funcionario'
---
-ALTER TABLE 'funcionario'
-MODIFY 'id' bigint(20) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table 'lancamento'
---
-ALTER TABLE 'lancamento'
-MODIFY 'id' bigint(20) NOT NULL AUTO_INCREMENT;
-
-INSERT INTO ` empresa ` ( ` id ` , ` cnpj ` , ` data_atualizacao ` , ` data_criacao ` , ` razao_social ` )
-VALUES ( NULL , '82198127000121' , CURRENT_DATE (), CURRENT_DATE (), 'Empresa
-ADMIN' );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+DROP TABLE IF EXISTS `lancamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lancamento` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `data_atualizacao` datetime NOT NULL,
+  `data_criacao` datetime NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `localizacao` varchar(255) DEFAULT NULL,
+  `tipo` varchar(255) DEFAULT NULL,
+  `funcionario_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`,`funcionario_id`),
+  KEY `fk_lancamento_funcionario1_idx` (`funcionario_id`),
+  CONSTRAINT `fk_lancamento_funcionario1` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
