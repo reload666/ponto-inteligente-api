2bdde682b6a3ba92e67510a718b4f53b26dca9d1
reload666 <piloto_66@hotmail.com>
2018-07-23 11:16:43 -0300

    Adicionado Query em LancamentoRepository

--- src/main/java/com/telefonica/pontointeligente/api/repositories/LancamentoRepository.java
+++ src/main/java/com/telefonica/pontointeligente/api/repositories/LancamentoRepository.java
@@ -2,6 +2,8 @@ package com.telefonica.pontointeligente.api.repositories;
 
 import java.util.List;
 
+import org.hibernate.annotations.NamedQueries;
+import org.hibernate.annotations.NamedQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 import com.telefonica.pontointeligente.api.entities.Lancamento;
 
 @Transactional(readOnly = true)
+@NamedQueries({
+	@NamedQuery(name = "LancamentoRepository.findByFuncionario",
+			query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionarioId")
+})
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
 	List<Lancamento> findByFuncionarioId(Long funcionarioId);
