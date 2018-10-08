f57b290d10838e0a0d4527a45b69974747716ef5
reload666 <piloto_66@hotmail.com>
2018-10-08 12:28:03 -0300

    Adicionado LancamentoController

---
+++ src/main/java/com/telefonica/pontointeligente/api/controllers/FuncionarioController.java
@@ -0,0 +1,124 @@
+package com.telefonica.pontointeligente.api.controllers;
+
+import java.math.BigDecimal;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.telefonica.pontointeligente.api.dto.FuncionarioDto;
+import com.telefonica.pontointeligente.api.entities.Funcionario;
+import com.telefonica.pontointeligente.api.response.Response;
+import com.telefonica.pontointeligente.api.services.FuncionarioService;
+import com.telefonica.pontointeligente.api.utils.PasswordUtils;
+
+@RestController
+@RequestMapping("/api/funcionarios")
+@CrossOrigin(origins = "*")
+public class FuncionarioController {
+
+	private static final Logger log = LoggerFactory.getLogger(FuncionarioController.class);
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	public FuncionarioController() {
+		super();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Response<FuncionarioDto>> atualizar(@PathVariable("id") Long id,
+			@Valid @RequestBody FuncionarioDto funcionarioDto, BindingResult result) throws NoSuchAlgorithmException {
+
+		log.info("Atualizando funcionario: {}", funcionarioDto.toString());
+		Response<FuncionarioDto> response = new Response<FuncionarioDto>();
+
+		Optional<Funcionario> funcionario = this.funcionarioService.buscarPorId(id);
+		if (!funcionario.isPresent()) {
+			result.addError(new ObjectError("funcionario", "Funcionário não encontrado."));
+		}
+
+		this.atualizarDadosFuncionarios(funcionario.get(), funcionarioDto, result);
+
+		if (result.hasErrors()) {
+			log.error("Erro validando funcionário: {}", result.getAllErrors());
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+
+		this.funcionarioService.persistir(funcionario.get());
+		response.setData(this.converterFuncionarioDto(funcionario.get()));
+
+		return ResponseEntity.ok(response);
+
+	}
+
+	/**
+	 * Retorna um DTO com os dados de um funcionário.
+	 * 
+	 * @param funcionario
+	 * @return FuncionarioDto
+	 */
+	private FuncionarioDto converterFuncionarioDto(Funcionario funcionario) {
+		FuncionarioDto funcionarioDto = new FuncionarioDto();
+		funcionarioDto.setId(funcionario.getId());
+		funcionarioDto.setEmail(funcionario.getEmail());
+		funcionarioDto.setNome(funcionario.getNome());
+		funcionario.getQtdHorasAlmocoOpt().ifPresent(
+				qtdHorasAlmoco -> funcionarioDto.setQtdHorasAlmoco(Optional.of(Float.toString(qtdHorasAlmoco))));
+		funcionario.getQtdHorasTrabalhoDiaOpt().ifPresent(qtdHorasTrabaloDia -> funcionarioDto
+				.setQtdHorasTrabalhoDia(Optional.of(Float.toString(qtdHorasTrabaloDia))));
+		funcionario.getValorHoraOpt().ifPresent(valorHora -> funcionarioDto.setValorHora(Optional.of(valorHora.toString())));
+
+		return funcionarioDto;
+	}
+
+	/**
+	 * Atualiza os dados do funcionário
+	 * 
+	 * @param funcionario
+	 * @param funcionarioDto
+	 * @param result
+	 * @throws NoSuchAlgorithmException
+	 */
+	private void atualizarDadosFuncionarios(Funcionario funcionario, @Valid FuncionarioDto funcionarioDto,
+			BindingResult result) throws NoSuchAlgorithmException {
+
+		funcionario.setNome(funcionarioDto.getNome());
+
+		if (!funcionario.getEmail().equals(funcionarioDto.getEmail())) {
+			this.funcionarioService.buscarPorEmail(funcionarioDto.getEmail())
+					.ifPresent(func -> result.addError(new ObjectError("email", "Email já existente.")));
+			funcionario.setEmail(funcionarioDto.getEmail());
+		}
+
+		funcionario.setQtdHorasAlmoco(null);
+		funcionarioDto.getQtdHorasAlmoco()
+				.ifPresent(qtdHoraAlmoco -> funcionario.setQtdHorasAlmoco(Float.valueOf(qtdHoraAlmoco)));
+
+		funcionarioDto.getQtdHorasTrabalhoDia()
+				.ifPresent(qtdHoraTrabalhoDia -> funcionario.setQtdHorasTrabalhoDia(Float.valueOf(qtdHoraTrabalhoDia)));
+
+		funcionario.setValorHora(null);
+		funcionarioDto.getValorHora().ifPresent(valorHora -> funcionario.setValorHora(new BigDecimal(valorHora)));
+
+		if (funcionarioDto.getSenha().isPresent()) {
+			funcionario.setSenha(PasswordUtils.gerarBCrypt(funcionarioDto.getSenha().get()));
+		}
+
+	}
+
+}
--- src/main/java/com/telefonica/pontointeligente/api/services/impl/LancamentoServiceImpl.java
+++ src/main/java/com/telefonica/pontointeligente/api/services/impl/LancamentoServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -18,33 +19,33 @@ public class LancamentoServiceImpl implements LancamentoService {
 	private static final Logger log = LoggerFactory.getLogger(Lancamento.class);
 
 	@Autowired
-	private LancamentoRepository LancamentoRepository;
+	private LancamentoRepository lancamentoRepository;
 
 	@Override
 	public Page<Lancamento> buscarPorFuncionarioId(Long funcionarioId, PageRequest pageRequest) {
 		log.info("Buscando lançamentos para o funcionario ID {}", funcionarioId);
-		return this.LancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
+		return this.lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest);
 	}
 
-	@Override
-	public Optional<Lancamento> buscarPorId(Long id) {
-		log.info("Buscando um lançamento pelo ID {}", id);
-		Optional<Lancamento> lancamento = Optional.empty();
-		lancamento = this.LancamentoRepository.findById(id);
-		
-		return lancamento;
-	}
+	 @Override
+	 public Optional<Lancamento> buscarPorId(Long id) {
+	 log.info("Buscando um lançamento pelo ID {}", id);
+	 Optional<Lancamento> lancamento = Optional.empty();
+	 lancamento = this.lancamentoRepository.findById(id);
+	
+	 return lancamento;
+	 }
 
 	@Override
 	public Lancamento persistir(Lancamento lancamento) {
 		log.info("Persistindo o lançamento: {}", lancamento);
-		return this.LancamentoRepository.save(lancamento);
+		return this.lancamentoRepository.save(lancamento);
 	}
 
 	@Override
 	public void remover(Long id) {
 		log.info("Removendo o lançamento ID {}", id);
-		this.LancamentoRepository.deleteById(id);
+		this.lancamentoRepository.deleteById(id);
 
 	}
 
